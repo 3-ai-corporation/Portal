e92521cb6cf4349a05f02a9b5b2c338da1ef9ab3
heytaro <nath.lo89@gmail.com>
2014-10-22 15:15:10 -0400

    Apaguei comentário nada profissional do código

--- service/model/ProfessoresModel.php
+++ service/model/ProfessoresModel.php
@@ -5,7 +5,6 @@ require_once 'settings/config.php';
 class ProfessoresModel extends ActiveRecord\Model {
 	static $table_name = 'tb_professores';
 	
-	//poe os nomes em pt_br sua biba
 	static $has_one = array (
 	   array("UsuariosModel")
 	);
