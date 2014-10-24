0da2e3df7b3884fcbc32e5afdcce9526a1cd9b92
Ana Karoliny Machado Macedo <anamachadomacedo@gmail.com>
2014-10-24 21:53:23 -0200

    #8 Criação de métodos

--- assets/js/desabilitandoBotoes.js
+++ assets/js/desabilitandoBotoes.js
@@ -3,28 +3,66 @@
  */
 
 //Criando um array estático (Como se o webservice - php - tivesse retornado esses valores);
-var serie = [];
-
-serie[0]['ae'] = [];
-serie[0]['ai'] = ['ED', 'aaa', 'bbb'];
-serie[0]['am'] = [];
-serie[0]['at'] = [];
-serie[1]['ae'] = [];
-serie[1]['ai'] = [];
-serie[1]['am'] = [];
-serie[1]['at'] = [];
-serie[2]['ae'] = [];
-serie[2]['ai'] = ['LPIII', 'ccc'];
-serie[2]['am'] = [];
-serie[2]['at'] = ['CD'];
-
+var materia = [];
+materia[0]['ae'] = [];
+materia[0]['ai'] = ['ED', 'aaa', 'bbb'];
+materia[0]['am'] = [];
+materia[0]['at'] = [];
+materia[1]['ae'] = [];
+materia[1]['ai'] = [];
+materia[1]['am'] = [];
+materia[1]['at'] = [];
+materia[2]['ae'] = [];
+materia[2]['ai'] = ['LPIII', 'ccc'];
+materia[2]['am'] = [];
+materia[2]['at'] = ['CD'];
 
 //Criando uma função que tem como parâmetros 'série' e 'turma', retornando disciplina;
 var getMateriasByTurma = function(t, s){
-    return serie[s][t];
+    return materia[t][s];
 }
 
-//Função que desabilita os botões;
+//pegar o ano selecionado, o curso selecionado. Pegar o vetor de matéria e verificar se o array é vazio ou não.
+var desativandoChecks = function(){
+    var vazio = true;
+    alert("testando");
+    for (i = 0; i < 3; i++){
+        if (materia[i]['ae'] !== []){
+            vazio = false;
+        }
+        if (materia[i]['ai'] !== []){
+            vazio = false;
+        }
+        if (materia[i]['am'] !== []){
+            vazio = false;
+        }
+        if (materia[i]['at'] !== []){
+            vazio = false;
+        }
+        if (vazio){
+            if (i === 0){
+                document.getElementById('ckbPrimeiro').isDisabled = true;
+            } else {
+                if (i === 1){
+                    document.getElementById('ckbSegundo').isDisabled = true;
+                } else{
+                    document.getElementById('ckbTerceiro').isDisabled = true;
+                }
+            }
+        }
+        vazio = true;
+    }
+}
+
+var desativandoBotoes = function(){
+    var vazio = true;
+    for (i = 0; i < 3; i++){
+        if (materia[i]['ae']){
+
+        }
+    }
+}
+
 
 
 
