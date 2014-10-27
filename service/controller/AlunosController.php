2b8ad3715c2571335504917d466f41b28f7e91c3
julianaCastro <juliana.cs97@gmail.com>
2014-10-27 08:49:52 -0400

    Criação do arquivo AlunosController
    [#13]

---
+++ service/controller/AlunosController.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'model/UsuariosModel.php';
+
+class AlunosController{
+
+	/*public function create($product){
+		$product = $this->object_to_array($product);
+		$product = ProductsModel::create($product);
+		return $product->to_array();
+	}
+*/
+	public function read($idTurmaSelecionada){
+		$alunos = UsuariosModel::find('all', array('conditions' => array("SELECT usuario.matricula, usuario.nome FROM tb_usuarios usuario
+																		WHERE usuario.matricula = tb_alunos.matricula
+																		AND tb_alunos.turma_id = ?", $idTurmaSelecionada), "order"=>"usuario.nome"));
+		
+		$retorno = array();
+		foreach ($alunos as $key => $value) {
+			$obj['matricula'] = $value->matricula;
+			$obj['nome'] = $value->nome;
+			$retorno[] = $obj;
+		}
+		
+		return $retorno;
+	}
+
+	
+
+	private function object_to_array(stdClass $Class){
+		$Class = (array)$Class;
+		foreach($Class as $key => $value){
+			if(is_object($value)&&get_class($value)==='stdClass'){
+				$Class[$key] = self::object_to_array($value);
+			}
+		}
+		return $Class;
+	}
+
+}
