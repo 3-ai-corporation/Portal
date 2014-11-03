b6769744036f162cd628bf98e3f3fec38963eb3c
jonathan <jonathan@Dom-pc>
2014-11-03 08:58:07 -0400

    [#15]Adicionando rota de acesso especializada, para pegar uma aula específica.

--- service/controller/DiasLetivosController.php
+++ service/controller/DiasLetivosController.php
@@ -21,6 +21,26 @@ class DiasLetivosController
 		}
 		return $retorno;
 	}
+	  	public function retrieveByIds ($Idsestrangeiro) 
+	{
+		$retorno = array();
+		$tempos = TemposModel::find('all',
+		array('conditions' => array("SELECT * FROM tb_tempos WHERE ( materia_turma_id = ? AND planos_id = ? )", $Idsestrangeiro.materia_turma_id, Idsestrangeiro.planos_id),
+		  "order"=>"indice"));
+		foreach($tempos as $key => $value) {
+			$obj['id'] = $value->id;
+			$obj['conteudo_previsto'] = $value->conteudo_previsdo;
+			$obj['conteudo_lancado'] = $value->conteudo_lancado;
+			$obj['indice'] = $value->indice;
+			$obj['reposicao'] = $value->reposicao;
+			$obj['idice'] = $value->indice;
+			$obj['letivos_id'] = $value->letivos_id;
+			$obj['planos_id'] = $value->planos_id;
+			$obj['materia_turma_id'] = $value->materia_turma_id;
+			$retorno[] = $obj;
+		}
+		return $retorno;
+	}
 
 
 	private function object_to_array($Class)
