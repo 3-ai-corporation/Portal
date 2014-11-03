9b5e3d545023b5a53143a5dbe628258e02cc37df
Nathália Oliveira <nath.lo89@gmail.com>
2014-11-02 22:33:30 -0400

    #7 Alterações na requisição do serviço

--- assets/js/diario_dataservice.js
+++ assets/js/diario_dataservice.js
@@ -1,112 +1,73 @@
-var diarioModulo = angular.module('diario', []);
 
-diarioModulo.controller('FiltroController', function ($scope, $modal, $log) {
-
-  //parte 1
-  
-  $.ajax({
+var professor = {};
+$.ajax({
           type: "GET",
-          url: 'service/diario-turma',
-          async: false,
+          url: 'service/diario-turmas',
+          /*async: false, ---> só adiciona essa linha de novo se der atraso na hora de carregar a página */
           success: function(data) {
-            $scope.items = jQuery.parseJSON(data);
+            professor = jQuery.parseJSON(data);
           }
     });
 
-  $scope.filtraTurma = function (size) {
-
-    var modalInstance = $modal.open({
-      templateUrl: 'portalModalContent',
-      controller: 'ModalInstanceCtrl',
-      size: size,
-      resolve: {
-        items: function () {
-            mat[0] = {};
-            mat[1] = {};
-            mat[2] = {};
-            for (mat in $scope.items) {
-              switch (mat.serie)
-              {
-                case 1: 
-                    switch (mat.cursos)
-                    {
-                        case 1:
-                            materia[0]['ae'] = mat.materia;
-                            break;
-                        case 2:
-                            materia[0]['ai'] = mat.materia;
-                            break;
-                        case 3:
-                            materia[0]['am'] = mat.materia;
-                            break;
-                        case 4:
-                            materia[0]['at'] = mat.materia;
-                            break;
-                    }
-                    break;
-                case 2: 
-                    switch (mat.cursos)
-                    {
-                        case 1:
-                            materia[1]['ae'] = mat.materia;
-                            break;
-                        case 2:
-                            materia[1]['ai'] = mat.materia;
-                            break;
-                        case 3:
-                            materia[1]['am'] = mat.materia;
-                            break;
-                        case 4:
-                            materia[1]['at'] = mat.materia;
-                            break;
-                    }
-                    break;
-                case 3: 
-                    switch (mat.cursos)
-                    {
-                        case 1:
-                            materia[2]['ae'] = mat.materia;
-                            break;
-                        case 2:
-                            materia[2]['ai'] = mat.materia;
-                            break;
-                        case 3:
-                            materia[2]['am'] = mat.materia;
-                            break;
-                        case 4:
-                            materia[2]['at'] = mat.materia;
-                            break;
-                    }
-                    break;
-              }
-          };
-          return materia;
+function filtraTurma(){
+  mat[0] = {};
+  mat[1] = {};
+  mat[2] = {};
+  for (mat in $scope.items) {
+    switch (mat.serie)
+    {
+      case 1: 
+        switch (mat.cursos)
+        {
+            case 1:
+                materia[0]['ae'] = mat.materia;
+                break;
+            case 2:
+                materia[0]['ai'] = mat.materia;
+                break;
+            case 3:
+                materia[0]['am'] = mat.materia;
+                break;
+            case 4:
+                materia[0]['at'] = mat.materia;
+                break;
         }
-      }
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-// Please note that $modalInstance represents a modal window (instance) dependency.
-// It is not the same as the $modal service used above.
-
-diarioModulo.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
-
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
-
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-});
+        break;
+      case 2: 
+        switch (mat.cursos)
+        {
+            case 1:
+                materia[1]['ae'] = mat.materia;
+                break;
+            case 2:
+                materia[1]['ai'] = mat.materia;
+                break;
+            case 3:
+                materia[1]['am'] = mat.materia;
+                break;
+            case 4:
+                materia[1]['at'] = mat.materia;
+                break;
+        }
+        break;
+      case 3: 
+        switch (mat.cursos)
+        {
+            case 1:
+                materia[2]['ae'] = mat.materia;
+                break;
+            case 2:
+                materia[2]['ai'] = mat.materia;
+                break;
+            case 3:
+                materia[2]['am'] = mat.materia;
+                break;
+            case 4:
+                materia[2]['at'] = mat.materia;
+                break;
+        }
+        break;
+    }
+  }
+        
+}
