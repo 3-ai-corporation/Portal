4116432ceef8766bbb19fdde8153e55248ce2d5f
jonathan <jonathan@Dom-pc>
2014-11-25 14:56:37 -0400

    [#15]Tentando mesclar no master o que fiz.

--- service/index.php
+++ service/index.php
@@ -6,21 +6,22 @@ require_once 'Slim/Slim/Slim.php';
 require_once 'controller/ProfessoresController.php';
 require_once 'controller/AlunosController.php';
 require_once 'controller/TemposController.php';
+require_once 'controller/DiasLetivosController.php';
 
 \Slim\Slim::registerAutoloader();
+
 $app = new Slim\Slim();
 
 $pcontroller = new ProfessoresController;
-$tcontroller = new TemposController;
-
-$acontroller = new AlunosController;
+$alunocontroller = new AlunosController;
+$temposcontroller = new TemposController;
+$aulascontroller = new DiasLetivosController;
 
 /*$app->get('/',function() use ($pcontroller) {
     echo json_encode($pcontroller->retrieveTurmas(134567, true));
 });*/
-
-$app->post('/service/plano-aula/update/',function($id) use ($tcontroller) {
-    echo json_encode($tcontroller->retrieve($id));
+$app->post('/service/plano-aula/update/',function($id) use ($temposcontroller) {
+    echo json_encode($temposcontroller->retrieve($id));
 });
 
 $app->get('/notify-recados',function() use ($pcontroller) {
@@ -33,11 +34,12 @@ $app->get('/notify-alunos',function() use ($pcontroller) {
 $app->get('/notify-portal',function() use ($pcontroller) {
     echo json_encode($pcontroller->getNotificacoesByCategory(123456,'portal'));
 });
-
+$app->get('/temposAula',function() use ($aulascontroller) {
+    echo json_encode($aulascontroller->retrieve());
+});
 $app->get('/alunosTurma',function() use ($acontroller) {
     echo json_encode($acontroller->retrieveAlunos(33));	
 });
-
 $app->get('/EsqueceuSenha/:matricula/:email',function($matricula, $senha) use ($pcontroller) {
     echo json_encode($pcontroller->retrieveSenha($matricula, $senha));	
 });
@@ -48,5 +50,7 @@ $app->get('/TrocarSenha/:matricula/:senha', function($matricula, $senha) use ($p
 $app->get('/Login/:matricula/:senha',function($matricula,$senha) use ($pcontroller) {
 	echo json_encode($pcontroller->login($matricula,$senha));
 });
+
 $app->run();
+
 ?>
