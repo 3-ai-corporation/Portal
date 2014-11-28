19e706d09d6762bb3265263005bd1461dbf5ef45
jonathan <jonathan@Dom-pc>
2014-11-28 15:44:34 -0400

    Revert "[#15]Função que retorna a série selecionada."
    This reverts commit 7a2cb617d031e2dcafb0a0791e2c7138a9ada509.

--- assets/js/ApoioFreq.js
+++ assets/js/ApoioFreq.js
@@ -6,36 +6,24 @@ function IniciarSelecao()
 	Ids_pegar.IdCurso = 1;
 	Ids_pegar.IdMateria = 1;		
 };
-//: deves colocar em turmaFiltro.php na div div'anopcoes->form'campoCheckBoxes'->input->>Onclick'[aqui]';
-var SelectedCurso = function(serie) {
-    var IdSerie;
-    if (serie.id === 'checkPrimeiro')
-        IdSerie = 0;
 
-    if (serie.id === 'checkSegundo')
-        IdSerie = 1;
-
-    if (serie.id === 'checkTerceiro')
-        IdSerie = 2;
-
-};
 function Selected_Serie()
 {
 	Ids_pegar.IdSerie = 1 ;
-};
+}
 
 function Selected_Materia()
 {
 	Ids_pegar.IdMateria = 1;
 
-};
+}
 
 function Selected_Curso()
 {
 	Ids_pegar.IdCurso = 1;
-};
+}
 
 function Selected_Bimestre()
 {
 	Ids_pegar.IdBimestre = 1;
-};
+}
