c3bf3a5e660259f898a10e0a8f5354b6da4fedc3
jonatasreis <jonatass.r@hotmail.com>
2014-12-01 21:23:23 -0200

    Criação do método para validar o código que foi enviado parao email do usuário

--- assets/js/Validacao.js
+++ assets/js/Validacao.js
@@ -310,3 +310,29 @@ function sendMail(matricula,email) {
 			return foi;
 		}		    
 	  });
+}
+
+function getCookie(cname) { //função do W3 Schools que retorna o cookie a partir do nome. Ver mais em: http://www.w3schools.com/js/js_cookies.asp
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function validar_codigo(codigo){
+	var code = getCookie("recoveryCode");
+    if (code !== "") { //quer dizer que o COOKIE do código está preenchido
+        if(codigo == parseInt(code)){
+			document.cookie = "recoveryCode=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+			return true;
+		}else{
+			return false;
+		}
+    } else {
+       showalert('erro','O código não existe ou expirou. Por favor, solicite novo envio e insira o código dentro de 15 minutos.');
+    }
+}
