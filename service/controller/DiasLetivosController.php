0c92aa3bbc55b01c946a5e24dc04413722b4a34e
CasmithValente <camilasmith2008@hotmail.com>
2014-11-30 21:23:33 -0400

    [#15] Mudanças na filtragem dos dias por bim
    eu tive que fazer as mudanças pq alguma criança abestada mudou os ids
    que eram chave estrangeira presente na tabela bimestre e colocou as
    datas na tora >:(.

--- service/controller/DiasLetivosController.php
+++ service/controller/DiasLetivosController.php
@@ -4,10 +4,11 @@ require_once 'model/DiasLetivosModel.php';
 require_once 'model/TurmasModel.php';
 require_once 'model/MateriasModel.php';
 require_once 'model/BimestresModel.php';
+require_once 'model/MateriaTurmasModel.php';
 
 class DiasLetivosController
 {
-	public function retrieve ($id_bim) 
+	public function retrieve ($id_bim,$id_turma,$id_materia)
 	{
 		/*inicio e fim do bimestre*/
 		$sel_bim = 'tb_bimestres.data_inicio_id AS inicio, tb_bimestres.data_fim_id AS fim';
@@ -20,28 +21,47 @@ class DiasLetivosController
 			$objBimestre['fim'] = $value->fim;			
 		}
 		
+		$dtI = explode('-',$objBimestre['inicio']);
+		$dtI = implode('_',$dtI);
 		
-		
-		//dia letivo de inicio e fim
+		$dtF = explode('-', $objBimestre['fim']);
+		$dtF = implode('_',$dtF);
+	
+		//--------------------------------------------------------------------------------------------
+		/*/dia letivo de inicio e fim
 		$sel_intervalo = 'DATE_FORMAT(tb_dia_letivos.data, "%d-%m-%Y") AS dt';
 		$inicio = DiasLetivosModel::find('all', array('select'=>$sel_intervalo, 'conditions'=>array('tb_dia_letivos.id = ?', $objBimestre['inicio'])));
 		$fim = DiasLetivosModel::find('all', array('select'=>$sel_intervalo, 'conditions'=>array('tb_dia_letivos.id = ?', $objBimestre['fim'])));
 		
 		foreach($inicio as $key=> $value){
-			$objInit['dt'] = $value->dt;	
+			$objInit['dt'] = $value->dt;
+			
 		}
-		
+		/*
 		$dtI = explode('-', $objInit['dt'] );
 		$dtI = implode ('_',$dtI);
-	
 		
 		foreach($fim as $key=> $value){
 			$objEnd['dt'] = $value->dt;		
 		}
 		$dtF = explode ('-',$objEnd['dt']);
 		$dtF = implode('_',$dtF);
+		//--------------------------------------------------------------------------------------------
+		*/
+		
+		//pesquisa de materia_turma_id
+		$sel_materia_turma = 'tb_materia_turmas.id AS id';
+		$materiaTurma = MateriaTurmasModel::find('all',array('select'=>$sel_materia_turma,'conditions'=>array('tb_materia_turmas.materia_id = ? AND tb_materia_turmas.turma_id = ?',$id_materia,$id_turma)));
 	
-		//todos os dias letivos
+		foreach($materiaTurma as $key => $value){
+			$objMateriaTurma['id'] = $value->id;
+		
+		}
+		
+		return $objMateriaTurma;
+		
+		
+		/*/todos os dias letivos
 		$sel_dias = 'tb_dia_letivos.id AS id, 	DATE_FORMAT(tb_dia_letivos.data, "%d-%m-%Y") AS datas, tb_dia_letivos.numero_dia AS numero_dia';
 		$diaAula = DiasLetivosModel::find('all', array('select'=>$sel_dias, 'order'=>'datas'));
 		$retorno_dias = array();
@@ -60,6 +80,7 @@ class DiasLetivosController
 			}
 		}
 		return $retorno_dias;
+		*/
 	}
 	
 		// Função que retorna os tempos -> ajeitar posteriormente
